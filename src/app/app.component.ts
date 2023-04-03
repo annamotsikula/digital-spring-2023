74109a2bed1f19acc5a166a49c7863a38b25f1d1
Anna <annamotsikula@gmail.com>
2023-04-03 21:30:06 +0400

    initial commit for Angular-intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IPerson } from './app.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tutor';
+  number: number = 15
+  
+  mySister: IPerson = {
+    fistName: 'Kate',
+    lastName: 'Smith'
+  }  
+
+  increment() {
+    console.log('You have clicked on this button')
+    this.number = this.number + 10
+  }
+  
+  
 }
+
+
---
+++ src/app/app.interface.ts
@@ -0,0 +1,5 @@
+export interface IPerson {
+    fistName: string,
+    lastName: string,
+    age?: number
+}
